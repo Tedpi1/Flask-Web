2abaa56ae1b948ad01bbc9a139bbf2021647788e
tedtony <moroted16@gmail.com>
2024-06-12 20:33:16 +0300

    stuck1

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+db=SQLAlchemy()
+DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY']='i have change a body'
+    app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
+    db.init_app(app) #initializing the database
     # SQLAlchemy Database Configuration With SQLite
     from .views import views
     from .auth import auth
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
+    from .models import User, Notes
+    create_database(app)
     return app
+def create_database(app):
+    if not path.exists('website/'+DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
