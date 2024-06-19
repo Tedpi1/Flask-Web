20ee15f616b254b1905faa9b956b88d296f3a05f
tedtony <moroted16@gmail.com>
2024-06-19 16:33:40 +0300

    still stuck in flask

--- website/__init__.py
+++ website/__init__.py
@@ -2,16 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 
+db=
 
-td = SQLAlchemy()
-DB_NAME = "database.td"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    td.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -19,12 +16,5 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
     
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
